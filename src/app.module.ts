3cc65176acb59d607b63aaae4cc6deba279cd6eb
GwangCheonLee <raitmid56@gmail.com>
2024-12-20 23:57:26 +0900

    chore: Add app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import {validationSchemaConfig} from './common/config/validation.config';
+import {getEnvPath} from './common/config/env-path.config';
+import {KafkaConsumerModule} from './kafka/consumer/kafka-consumer.module';
+import {KafkaProducerModule} from './kafka/producer/kafka-producer.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {TypeormConfig} from './common/config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    KafkaProducerModule,
+    KafkaConsumerModule,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfig,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: getEnvPath(),
+      validationSchema: validationSchemaConfig(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
